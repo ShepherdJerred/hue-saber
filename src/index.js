8914b4cc81d488693fc91ccaa58c8e19291443ed
Jerred Shepherd <ShepherdJerred@gmail.com>
2020-11-05 17:12:56 -0800

    Restart

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import WebSocket from 'ws';
-import * as hue from 'node-hue-api';
-import * as dotenv from 'dotenv';
-import low from 'lowdb';
-import FileSync from 'lowdb/adapters/FileSync';
-
-dotenv.config();
-const adapter = new FileSync('db.json');
-
